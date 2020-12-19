e5a87994168ec6c77fc6cd1639639883efa3061f
Lukas Jurcaga <lj2003@hw.ac.uk>
2020-12-19 22:58:40 +0000

    Initial commit

---
+++ app/src/main/java/com/example/nfc_tag_app_test/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.nfc_tag_app_test;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
